efd4caea2cd68f072a7e2b0bef2331cfa1434023
Zainab Khayat <zozok22498@gmail.com>
2024-05-15 22:57:15 +0300

    Update DashboardTab.jsx

--- src/pages/admin/dashboard/DashboardTab.jsx
+++ src/pages/admin/dashboard/DashboardTab.jsx
@@ -1,5 +1,4 @@
 import React, { useContext, useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import myContext from '../../../context/data/myContext';
 import Layout from '../../../components/layout/Layout';
